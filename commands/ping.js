d2f982ec62cec94f33132816bd0aa619770b9999
Anton Van Assche <vanasscheanton@gmail.com>
2021-04-04 13:11:35 +0200

    Added ping command

---
+++ commands/ping.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "ping",
+    aliases: ["latency"],
+    run: async (client, message, args) => {
+        const embed = new Discord.MessageEmbed()
+            .setTitle("Ping")
+            .setDescription(`🏓 Bot latency is ${Date.now() - message.createdTimestamp}ms.`)
+        message.channel.send(embed)
+    }
+}
