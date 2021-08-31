3b70cc05e112405195ebe902dfd6ef0138f5edb9
Anton Van Assche <vanasscheanton@gmail.com>
2021-08-31 14:50:14 +0200

    Updated OS check.

--- setup.sh
+++ setup.sh
@@ -6,29 +6,30 @@ cyan="\e[96m"
 red="\e[91m"
 reset="\e[0m"
 
-# RELEASE
-RELEASE="$(lsb_release -is)"
+# System checks
+OS="$(hostnamectl | grep "Operating System: " | sed -e "s/Operating System: //")"
 
-# Check if OS is supported (Raspbian, Ubuntu, Debian)
-if [[ $RELEASE == "Raspbian" || $RELEASE == "Ubuntu" || $RELEASE == "Debian" ]]; then
-
-    echo
-    echo -e "[i] ${cyan}$RELEASE is supported${reset}"
-
-else
-
-    echo
-    echo -e "[!] ${red}$RELEASE is not supported${reset}"
-    echo
-    exit 1
-
-fi
+case $OS in
+   *"Ubuntu"*)
+      message info "Ubuntu detected, which is supported."
+      ;;
+   *"Raspbian"*)
+      message info "Raspbian detected, which is supported."
+      ;;
+   *"Debian"*)
+      message info "Debian detected, which is supported."
+      ;;
+   *)
+      message error "This system is not supported."
+      exit 1
+      ;;
+esac
 
 cd $HOME/music-bot
 
 # Installing Screen
 sudo apt update
-sudo apt install screen
+sudo apt install screen -y
 
 # Installing dependencies
 npm install discord.js && npm install ffmpeg-static && npm install @discordjs/opus && npm i distube
@@ -37,3 +38,4 @@ npm install discord.js && npm install ffmpeg-static && npm install @discordjs/op
 screen -dm -S music-bot ./startup.sh
 
 echo -e "[i] ${cyan}Use this command to start the bot in a new screen session: screen -dm -S music-bot $HOME/music-bot/startup.sh${reset}"
+
