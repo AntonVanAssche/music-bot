99a81ab9f6736ee4a336ea539c927251a8249c4b
Anton <vanasscheanton@gmail.com>
2022-11-02 13:26:59 +0100

    Set indentation to 4 spaces

--- commands/ping.js
+++ commands/ping.js
@@ -1,12 +1,12 @@
 const Discord = require("discord.js")
 
 module.exports = {
-    name: "ping",
-    aliases: ["latency"],
-    run: async (client, message, args) => {
-        const embed = new Discord.MessageEmbed()
-            .setTitle("Ping")
-            .setDescription(`🏓 Bot latency is ${Date.now() - message.createdTimestamp}ms.`)
-        message.channel.send(embed)
-    }
+     name: "ping",
+     aliases: ["latency"],
+     run: async (client, message, args) => {
+          const embed = new Discord.MessageEmbed()
+                .setTitle("Ping")
+                .setDescription(`🏓 Bot latency is ${Date.now() - message.createdTimestamp}ms.`)
+          message.channel.send(embed)
+     }
 }
